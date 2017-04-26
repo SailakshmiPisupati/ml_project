0764dbea550bb92b893116828b6a62f6abdf17a0
Naveen Bansal <naveen18jan93@gmail.com>
2017-04-26 00:07:20 -0400

    Used inbuilt function to split data in training and test set. Added more classifiers

--- perceptron.py
+++ perceptron.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pydotplus
 from StringIO import StringIO
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
-from sklearn import tree
+from sklearn.linear_model import perceptron
 
 with open('voice.csv') as voicefile:
 	myFileReader = csv.reader(voicefile, delimiter=',', quotechar='|')
@@ -22,12 +23,9 @@ with open('voice.csv') as voicefile:
 	# removing column names
 	del(X[0]) 
 	del(Y[0])
+	X = preprocessing.scale(X) # attribute scaling so that attributes with high values do not dominate
 	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-	clf = tree.DecisionTreeClassifier()
-	clf = clf.fit(X_train, y_train)
+	clf = perceptron.Perceptron(penalty='l1', n_iter=50, verbose=0, random_state=None, fit_intercept=True, eta0=0.02)
+	clf.fit(X_train, y_train)
 	scores = cross_val_score(clf, X_test, y_test, cv=5)
 	print(scores.mean())
-	dotfile = StringIO()
-	tree.export_graphviz(clf, out_file = dotfile, class_names = ['-', '+'], rounded = True)  #, feature_names = ['1', '0']
-	graph = pydotplus.graph_from_dot_data(dotfile.getvalue())
-	graph.write_pdf("clf1.pdf")
